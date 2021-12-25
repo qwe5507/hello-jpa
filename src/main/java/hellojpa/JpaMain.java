928330237f81751f6fda6131d8b16268ab274d5e
qwe5507 <apvmffkdls@naver.com>
2021-12-25 18:40:39 +0900

    Initial commit

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,35 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpafirst");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        //code
+        try {
+            List<Member> result = em.createQuery("select m from Member as m where m.id >= 2", Member.class)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
