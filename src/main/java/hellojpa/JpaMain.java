6f0ebbfcaae37b0bd5b59617f62f295967d3808c
qwe5507 <apvmffkdls@naver.com>
2021-12-28 21:46:31 +0900

    데이터베이스 스키마 자동 생성

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,12 +18,14 @@ public class JpaMain {
         try {
             //영속 상태
             Member member = em.find(Member.class, 250L);
-            member.setName("AAAAA");
+            System.out.println("member = " + member.getId());
+            System.out.println("member = " + member.getName());
+//            member.setName("AAAAA");
 
 //            em.detach(member); //jpa에서 관리 안함
-            em.clear(); // 영속성 컨텍스트 통째로 삭제
+//            em.clear(); // 영속성 컨텍스트 통째로 삭제
 
-            Member member2 = em.find(Member.class, 250L);
+//            Member member2 = em.find(Member.class, 250L);
 
             System.out.println("=========");
             tx.commit();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,20 +1,25 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity
+@Entity(name = "mem")
+@Table(name = "Member")
 public class Member {
     @Id
     private Long id;
+    @Column(unique = true, length = 10)
     private String name;
+    private int age;
+
     public Member() {
     }
     public Member(Long id, String name) {
         this.id = id;
         this.name = name;
     }
-
     public Long getId() {
         return id;
     }
