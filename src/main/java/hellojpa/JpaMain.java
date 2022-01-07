9ce71e2f2fa95af01c485b4b2c4a3fe58843f64c
qwe5507 <apvmffkdls@naver.com>
2022-01-08 08:48:08 +0900

    프록시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,8 @@
 package hellojpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -19,24 +18,23 @@ public class JpaMain {
             member1.setUsername("member1");
             em.persist(member1);
 
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            em.persist(member2);
-
             em.flush();
             em.clear();
 
-            Member m1 = em.find(Member.class, member1.getId());
-            Member m2 = em.find(Member.class, member2.getId());
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("refMember = " + refMember.getClass()); //Proxy
 
-            System.out.println("m1 == m2 : " + (m1.getClass() == m2.getClass()));
-            System.out.println("m1 : " + m1);
-            System.out.println("m1 : " + m1.getClass());
+            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember));
 
+            Hibernate.initialize(refMember); //강제 초기화
+//            refMember.getUsername(); //프록시 초기화, 쿼리 발생 // 강제 초기화
+
+            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember));
 
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
+            e.printStackTrace();
         } finally {
             em.close();
         }
